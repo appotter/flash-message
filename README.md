db74f3a8e50ee109031274345c59db6da7a7a1b1
AP Potter <iamhottest2pm@gmail.com>
2015-11-22 16:52:41 +0700

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# flash-message
-The Flash message with your project
+# Flash Message
+
+This is a simple library which has two static methods allowing you to get/set flash data or messages.
+
+Coded by Joel Vardy.
+
+Reformer by Phitsanu AP Chuamuangphan
+
+## Installation
+
+Add the following to your `composer.json` require object:
+
+```json
+"appotter/flash-message": "~1.0"
+```
+
+## Usage
+
+### Data
+
+Set data upon failure.
+
+```php
+Flash::data([
+	'username' => $username,
+	'email'    => $email
+]);
+```
+
+Read data to display on the front end:
+
+```php
+$data = Flash::data();
+```
+
+### Messages
+
+Set an error message:
+
+```php
+Flash::message('error', 'You must enter a valid email address.');
+```
+
+Read all flash messages:
+
+```php
+$messages = Flash::message();
+```
---
+++ src/Appotter/Flash.php
@@ -0,0 +1,70 @@
+<?php
+namespace Appotter;
+
+/**
+ * Flashdata library
+ *
+ * @link	https://github.com/appotter/flash-message
+ * @author	Joel Vardy <info@joelvardy.com>
+ * @reformer Phitsanu AP Chuamuangphan <ap.phitsanu@gmail.com>
+ */
+class Flash
+{
+	// TODO: Allow this to be set
+	protected static $session_name = 'flash';
+
+	/**
+	 * Get/set flash data
+	 *
+	 * @return	mixed|void
+	 */
+	public static function data($payload = null)
+	{
+		// Overwrite payload
+		if ($payload) {
+
+			$_SESSION[self::$session_name]['data'] = $payload;
+
+			return;
+		}
+
+		// Read data
+		$data = false;
+
+		if (isset($_SESSION[self::$session_name]['data']) {
+			$data = $_SESSION[self::$session_name]['data'];
+		}
+
+		unset($_SESSION[self::$session_name]['data']);
+
+		return $data;
+	}
+
+	/**
+	 * Get/set flash message
+	 *
+	 * @return	array|void
+	 */
+	public static function message($type = null, $message = null)
+	{
+		// Add new message
+		if ($type and $message) {
+
+			$_SESSION[self::$session_name]['messages'][$type][] = $message;
+
+			return;
+		}
+
+		// Read messages
+		$messages = array();
+
+		if (isset($_SESSION[self::$session_name]['messages']) {
+			$messages = $_SESSION[self::$session_name]['messages'];
+		}
+
+		unset($_SESSION[self::$session_name]['messages']);
+
+		return $messages;
+	}
+
+}
